a5ad587fa1b80cdb000153acb38b23bfc74a0f4b
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-16 20:05:46 +0000

    Convert params to number in getEventDate function

--- common.mjs
+++ common.mjs
@@ -26,21 +26,24 @@ const weeksArr = [
 
 const occurrenceNumber = { first: 1, second: 2, third: 3, last: -1 };
 
-function getEventDate(year, month, dayOfWeek, occurrence) {
-  if (occurrence > 0) {
+export function getEventDate(year, month, dayOfWeek, occurrence) {
+  const monthIndex = monthsArr.indexOf(month);
+  const weekIndex = weeksArr.indexOf(dayOfWeek);
+  const occurrenceIndex = occurrenceNumber[occurrence];
+  if (occurrenceIndex > 0) {
     // If occurrence is first, second or third
-    const firstWeekdayOfMonth = new Date(year, month, 1).getDay(); // Gets the weekday for the 1st day of the month
-    let day = 1 + ((dayOfWeek - firstWeekdayOfMonth + 7) % 7); // Difference between day of the week and 1st day of the month's week
-    day += (occurrence - 1) * 7; // Gets the date of the month
-    return new Date(year, month, day, 3); // I have add the hour as 3 to avoid problems with daylight saving
+    const firstWeekdayOfMonth = new Date(year, monthIndex, 1).getDay(); // Gets the weekday for the 1st day of the month
+    let day = 1 + ((weekIndex - firstWeekdayOfMonth + 7) % 7); // Difference between day of the week and 1st day of the month's week
+    day += (occurrenceIndex - 1) * 7; // Gets the date of the month
+    return new Date(year, monthIndex, day, 3); // I have add the hour as 3 to avoid problems with daylight saving
   } else {
     // If occurrence is last
-    const lastDayOfMonth = new Date(year, month + 1, 0); // Gets the last day of the month
+    const lastDayOfMonth = new Date(year, monthIndex + 1, 0); // Gets the last day of the month
     const lastDate = lastDayOfMonth.getDate(); // Gets the date from the last day of the month
     const lastDayOfWeek = lastDayOfMonth.getDay();
-    let day = lastDate - ((lastDayOfWeek - dayOfWeek + 7) % 7);
-    day += (occurrence + 1) * 7;
-    return new Date(year, month, day, 3);
+    let day = lastDate - ((lastDayOfWeek - weekIndex + 7) % 7);
+    day += (occurrenceIndex + 1) * 7;
+    return new Date(year, monthIndex, day, 3);
   }
 }
 
