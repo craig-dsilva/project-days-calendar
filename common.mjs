4d99b56d3efaf002d05f671278d2e06bd3f6d097
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-16 17:41:48 +0000

    Add getEventDate function

--- common.mjs
+++ common.mjs
@@ -1,5 +1,17 @@
-// This is a placeholder file which shows how you can define functions which can be used from both a browser script and a node script. You can delete the contents of the file once you have understood how it works.
-
-export function getGreeting() {
-    return "Hello";
+function getEventDate(year, month, dayOfWeek, occurrence) {
+  if (occurrence > 0) {
+    // If occurrence is first, second or third
+    const firstWeekdayOfMonth = new Date(year, month, 1).getDay(); // Gets the weekday for the 1st day of the month
+    let day = 1 + ((dayOfWeek - firstWeekdayOfMonth + 7) % 7); // Difference between day of the week and 1st day of the month's week
+    day += (occurrence - 1) * 7; // Gets the date of the month
+    return new Date(year, month, day, 3);
+  } else {
+    // If occurrence is last
+    const lastDayOfMonth = new Date(year, month + 1, 0); // Gets the last day of the month
+    const lastDate = lastDayOfMonth.getDate(); // Gets the date from the last day of the month
+    const lastDayOfWeek = lastDayOfMonth.getDay();
+    let day = lastDate - ((lastDayOfWeek - dayOfWeek + 7) % 7);
+    day += (occurrence + 1) * 7;
+    return new Date(year, month, day, 3);
+  }
 }
