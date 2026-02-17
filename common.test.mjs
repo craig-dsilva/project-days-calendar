db9b18c501c644c216cdc4c06a1236badd1a590f
Craig D'Silva <craig.dsilva10@gmail.com>
2026-02-17 10:02:42 +0000

    Remove import

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
 import { getEventDate } from "./common.mjs";
-import { get } from "node:http";
 
 describe("getEventDate", () => {
   const event_ = {
